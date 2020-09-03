eeda9fa30133ae95bc3c622eb84bee1a2722b55c
FredrikM2000 <70491657+FredrikM2000@users.noreply.github.com>
2020-09-03 02:14:25 +0200

    almost done

--- Task 5.cpp
+++ Task 5.cpp
@@ -2,7 +2,31 @@
 
 int main()
 {
-    for (int i = 1;i <= 5;i++) {
-        std::cout << "\t" << i;
+    for (int i = 1;i <= 6;i++) {
+        std::cout << "     " << i << "\t";
+    }
+    for (int t = 1; t <= 4;t++) {
+        std::endl(std::cout);
+        for (int j = 1;j <= 6; j++) {
+            std::cout << "   _ _ _";
+        }
+        std::endl(std::cout);
+        for (int n = 1;n <= 6;n++) {
+            std::cout << " |      ";
+        }
+        std::cout << " |";
+        std::endl(std::cout);
+        for (int k = 1;k <= 6;k++) {
+            std::cout << " |   *  ";
+        }
+        std::cout << " |";
+    }
+    std::endl(std::cout);
+    for (int j = 1;j <= 6; j++) {
+        std::cout << "   _ _ _";
+    }
+    std::endl(std::cout);
+    for (int i = 1;i <= 6;i++) {
+         std::cout << "     " << i << "\t";
     }
 }
